edfbcf3fbfe194a2004c449e5e8af7f691db3cad
hideni21 <37110829+hideni21@users.noreply.github.com>
2021-08-01 22:53:46 +0700

    Delete AlfaGaming.cpp

--- AlfaGaming.cpp
+++
@@ -1,9 +0,0 @@
-{
-"Version": 4,
-"Creator": "Alfa Gaming",
-"Title": "Hallo Guys,",
-"Info": "Yang Mau Coba Mod Menu Nya Bisa Ke Channel Alfa Gaming",
-"Changelogs": "Jadi Subscribe Alfa Gaming No Lama",
-"Link": "https://youtube.com/channel/UCsqEoXuciJLKtYzFlyczD7Q",
-"Premium": false
-}
