80900b389da0b34d483acb7f4d1363b9f57d1a33
hideni21 <37110829+hideni21@users.noreply.github.com>
2021-08-01 22:52:56 +0700

    Update AlfaGaming.cpp

--- AlfaGaming.cpp
+++ AlfaGaming.cpp
@@ -1,9 +1,9 @@
 {
 "Version": 4,
-"Creator": "PowerKuy",
-"Title": "Hello Guys,",
-"Info": "Never Download Mod Menu From Other Channel\nBecause its Stealer\nNever Download From Channel That Review My Mod Menu , Never Download It\nJust Download Original In My YT",
-"Changelogs": "Btw Just Wait Im Update\nClick Link Below , Its Real Channel YT",
-"Link": "https://youtube.com/c/PowerKuy",
+"Creator": "Alfa Gaming",
+"Title": "Hallo Guys,",
+"Info": "Yang Mau Coba Mod Menu Nya Bisa Ke Channel Alfa Gaming",
+"Changelogs": "Jadi Subscribe Alfa Gaming No Lama",
+"Link": "https://youtube.com/channel/UCsqEoXuciJLKtYzFlyczD7Q",
 "Premium": false
 }
